3f2e1d898e1fbf1a45f6467a99dd43ca3423e3a0
mario <mario.rincon.nigro@gmail.com>
2017-01-27 19:30:17 +0100

    Leetcode 389 improved

--- leetcode/389_find_the_difference.cpp
+++ leetcode/389_find_the_difference.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int count['z' - 'a' + 1] = {0};
+        char diff = (char)0;
         
         for(int i = 0; i < s.size(); i++)
-            count[s[i] - 'a']++;
+            diff ^= s[i];
             
         for(int i = 0; i < t.size(); i++)
-            count[t[i] - 'a']--;
+            diff ^= t[i];
             
-        for(int i = 0; i < 'z' - 'a' + 1; i++)
-            if(count[i])
-                return 'a' + i;
+        return diff;
     }
 };
