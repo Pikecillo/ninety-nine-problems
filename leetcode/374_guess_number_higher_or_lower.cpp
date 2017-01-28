d6b144905e15118b673abdfc6a95529cc2698a0f
mario <mario.rincon.nigro@gmail.com>
2017-01-28 23:19:58 +0100

    Leetcode 374

---
+++ leetcode/374_guess_number_higher_or_lower.cpp
@@ -0,0 +1,24 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long lo = 0, hi = n, num, cmp;
+        
+        do {
+            num = (lo + hi) / 2;
+            cmp = guess(num);
+            
+            if(cmp == -1)
+                hi = num;
+            if(cmp == 1)
+                lo = num + 1;
+            
+        } while(cmp != 0);
+        
+        return num;
+    }
+};
